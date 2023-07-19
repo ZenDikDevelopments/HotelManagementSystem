a1862a8b74712c665a701b478b2790fb0b106449
LyuboDikov <lyubo.dikov@icloud.com>
2023-07-19 12:01:09 +0300

    Added Repository annotations

--- HotelManagementSystem/src/main/java/com/zendikdevelopments/hotelmanagementsystem/repositories/RoleRepository.java
+++ HotelManagementSystem/src/main/java/com/zendikdevelopments/hotelmanagementsystem/repositories/RoleRepository.java
@@ -2,6 +2,8 @@ package com.zendikdevelopments.hotelmanagementsystem.repositories;
 
 import com.zendikdevelopments.hotelmanagementsystem.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- HotelManagementSystem/src/main/java/com/zendikdevelopments/hotelmanagementsystem/repositories/UserRepository.java
+++ HotelManagementSystem/src/main/java/com/zendikdevelopments/hotelmanagementsystem/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.zendikdevelopments.hotelmanagementsystem.repositories;
 
 import com.zendikdevelopments.hotelmanagementsystem.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
